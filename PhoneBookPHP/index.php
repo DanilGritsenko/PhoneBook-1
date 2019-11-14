933b13c1f77e06657de4ef88133f0b313ba1e033
Opilane TTHK <opilane@tthk.ee>
2019-11-14 14:25:35 +0200

    3 функция

--- PhoneBookPHP/index.php
+++ PhoneBookPHP/index.php
@@ -99,7 +99,10 @@ function searchByNumber($query){
 			
             ?>
         </table>
-		
+		<h2>Esimene nimi XML failis</h2>
+		<?php
+			echo "<h4>".$computers -> kontakt -> nimi[0]."</h4>";
+		?>
 
     </body>
 </html>
