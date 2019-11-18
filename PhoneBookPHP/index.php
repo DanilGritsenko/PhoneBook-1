92680ade84de8037dc3088148083eb4e6c9d4519
Opilane TTHK <opilane@tthk.ee>
2019-11-18 11:21:22 +0200

    Сортировка v1.0

--- PhoneBookPHP/index.php
+++ PhoneBookPHP/index.php
@@ -13,6 +13,7 @@ function searchByName($query){
     }
     return $result;
 }
+
 function searchByNumber($query){
     global $computers;
     $result = array();
@@ -22,15 +23,60 @@ function searchByNumber($query){
     }
     return $result;
 }
+
+function searchBySurname($query){
+    global $computers;
+    $result = array();
+    foreach ($computers -> kontakt as $computer){
+        if (substr(strtolower($computer -> perekonnanimi), 0, strlen($query))==strtolower($query))
+            array_push($result, $computer);
+    }
+    return $result;
+}
+
+function searchByEmail($query){
+    global $computers;
+    $result = array();
+    foreach ($computers -> kontakt as $computer){
+        if (substr(strtolower($computer -> email), 0, strlen($query))==strtolower($query))
+            array_push($result, $computer);
+    }
+    return $result;
+}
+
+$computer = array();
+foreach($computers->kontakt as $item) {
+    $computer[] = array(
+                     'id'             => (string)$item->id,
+                     'name'          => (string)$item->name,
+                     'surname'           => (string)$item->surname,
+                     'number'         => (string)$item->number,
+                     'email' => (string)$item->email
+                    );
+}
+array_sort_by_column($computer, 'Name');
+var_dump($computer);
+
+function array_sort_by_column(&$array, $column, $direction = SORT_ASC) {
+    $reference_array = array();
+
+    foreach($array as $key => $row) {
+        $reference_array[$key] = $row[$column];
+    }
+
+    array_multisort($reference_array, $direction, $array);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>XML Reading</title>
+        <title>Telefoniraamat</title>
         <meta charset="utf-8">
       <link rel="stylesheet" type="text/css" href="skeleton.css">
     </head>
     <body>
+	<input type="submit" name="sort" value="Sort" onClick="array_sort_by_column">
         <h1>Telefoniraamat</h1>
        
         <table border="1">
@@ -55,16 +101,17 @@ function searchByNumber($query){
             
             ?>
         </table>
-
-        
-
         <br />
-        <form action="?" method="post">
+		
+        <form method="post">
             Search: <input type="text" name="search"/>
-			Otsing nimi jargi<input type="radio" name="radiofind"  value="name" checked>
-			Otsing telefoni numbri jargi<input type="radio" name="radiofind"  value="num">
+			Otsing nimi järgi<input type="radio" name="radiofind"  value="name" checked>
+			Otsing perekonnanimi järgi<input type="radio" name="radiofind"  value="surname">
+			Otsing telefoni numbri järgi<input type="radio" name="radiofind" value="num">
+			Otsing emaili järgi<input type="radio" name="radiofind" value="email">
             <input type="submit" value="Find" />
         </form>
+		
         <table border="1">
          <tr>
                 <th>Nimi</th>
@@ -83,6 +130,13 @@ function searchByNumber($query){
 				else if($answer == "num"){
 					$result = searchByNumber($_POST["search"]);
 				}
+				else if($answer == "surname"){
+					$result = searchBySurname($_POST["search"]);
+				}
+				else if($answer == "email"){
+					$result = searchByEmail($_POST["search"]);
+				}
+				
 			
             foreach($result as $arvuti) {
 				$counter++;
@@ -99,10 +153,12 @@ function searchByNumber($query){
 			
             ?>
         </table>
+		
 		<h2>Esimene nimi XML failis</h2>
 		<?php
 			echo "<h4>".$computers -> kontakt -> nimi[0]."</h4>";
 		?>
+		
 
     </body>
 </html>
